4e85b8db1a1058c80b4065a7a27deddbc7f3d431
saywordsahn <saywordsahn@gmail.com>
2025-01-04 02:21:19 +0900

    Added load_strip function to SpriteSheet class for easier animation loading

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 urban_theme_image = pygame.image.load('./Sprites/UrbanTheme.png')
 
 
-sprite_sheet = SpriteSheet(urban_theme_image, (32, 32))
+sprite_sheet = SpriteSheet(urban_theme_image, (32, 32), 7, 8)
 
 image = sprite_sheet.get_image(0, 0, 64, 64)
+
+ground_images = sprite_sheet.load_strip((4, 0), 8, 64, 64)
 
 
 while True:
 
     screen.fill((50,50,50))
-    screen.blit(image, (0, 0))
+
+    for i in range(len(ground_images)):
+        screen.blit(ground_images[i], (i * 64, 0))
+
 
 
     for event in pygame.event.get():
--- spritesheet.py
+++ spritesheet.py
@@ -2,14 +2,41 @@ import pygame
 
 class SpriteSheet:
 
-    def __init__(self, image, cell_size):
+    def __init__(self, image, cell_size, num_rows, num_cols):
         self.image = image
         self.cell_size = cell_size
+        self.num_rows = num_rows
+        self.num_cols = num_cols
 
     def get_image(self, row, col, width, height):
         image = pygame.Surface((self.cell_size[0], self.cell_size[1])).convert_alpha()
-        image.blit(self.image, (0, 0), (row * self.cell_size[0], col * self.cell_size[1], self.cell_size[0], self.cell_size[1]))
+        image.blit(self.image, (0, 0), (col * self.cell_size[0], row * self.cell_size[1], self.cell_size[0], self.cell_size[1]))
 
         image = pygame.transform.scale(image, (width, height))
         image.set_colorkey((0, 0, 0))
         return image
+
+    def load_strip(self, start, num_frames, width, height):
+
+        strip = []
+
+        row = start[0]
+        col = start[1]
+
+        while num_frames >= 0:
+
+            if col >= self.num_cols:
+                row += 1
+                col = 0
+            else:
+                image = self.get_image(row, col, width, height)
+                strip.append(image)
+                col += 1
+                num_frames -= 1
+
+        return strip
+
+
+
+
+
