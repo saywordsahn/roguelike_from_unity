e4a2afd4b7a9518173fb48368d209f2a7afa9e39
saywordsahn <saywordsahn@gmail.com>
2025-01-03 22:36:48 +0900

    Added function to get an image from a sprite sheet

--- main.py
+++ main.py
@@ -9,12 +9,29 @@ SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
+urban_theme_image = pygame.image.load('./Sprites/UrbanTheme.png')
+
+
+def get_image(sprite_sheet, row, col, width, height):
+    image = pygame.Surface((width, height)).convert_alpha()
+    image.blit(sprite_sheet, (0, 0), (row * width, col * width, width, height))
+
+    image = pygame.transform.scale(image, (64, 64))
+    image.set_colorkey((0, 0, 0))
+    return image
+
+
 while True:
+
+    screen.fill((50,50,50))
+    screen.blit(get_image(urban_theme_image, 1, 1, 32, 32), (0, 0))
+
 
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            quit(0)
+            pygame.quit()
+            exit()
 
 
     pygame.display.update()
