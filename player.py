dee5e16bd7250a2f9a2bd062f9e040a77a297236
saywordsahn <saywordsahn@gmail.com>
2025-01-07 17:08:57 +0900

    Converted player's grid position to pygame.Vector2

--- player.py
+++ player.py
@@ -1,31 +1,35 @@
 from settings import *
+import pygame
 
 class Player:
 
     def __init__(self, idle_animation):
-        self.row = 1
-        self.col = 1
+        self.grid_position = pygame.Vector2(1, 1)
         self.idle_animation = idle_animation
         self.state = PlayerState.IDLE
+        self.move_speed = 3
 
     def draw(self, screen) -> None:
-        screen.blit(self.idle_animation.get_current_image(), grid_to_world(self.row, self.col))
+        screen.blit(self.idle_animation.get_current_image(), grid_to_world(self.grid_position))
 
     def update(self, dt):
         self.idle_animation.update(dt)
 
-        if self.state == PlayerState.MOVING:
-            print('Im moving')
+        # if self.state == PlayerState.MOVING:
+        #     self.col += self.move_speed * dt / 1000.0
+
 
     def move(self, direction: Direction) -> None:
 
         if direction == Direction.RIGHT:
-            self.col += 1
+            self.grid_position += Vector2.RIGHT
         elif direction == Direction.LEFT:
-            self.col -= 1
+            self.grid_position += Vector2.LEFT
         elif direction == Direction.UP:
-            self.row -= 1
+            self.grid_position += Vector2.UP
         else:
-            self.row += 1
+            self.grid_position += Vector2.DOWN
+
+        self.state = PlayerState.MOVING
 
 
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import enum
 from typing import Tuple
+import pygame
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 512
@@ -18,5 +19,18 @@ class PlayerState(enum.Enum):
     MOVING = 2,
     ATTACKING = 3
 
-def grid_to_world(row, col) -> Tuple[int, int]:
-    return col * TILE_SIZE[0] * SCALE, row * TILE_SIZE[1] * SCALE
+class Vector2:
+
+    RIGHT = pygame.Vector2(0, 1)
+    LEFT = pygame.Vector2(0, -1)
+    UP = pygame.Vector2(-1, 0)
+    DOWN = pygame.Vector2(1, 0)
+
+
+def grid_to_world(grid_position) -> Tuple[float, float]:
+
+    print(grid_position.x, grid_position.y)
+    return grid_position.y * TILE_SIZE[0] * SCALE, grid_position.x * TILE_SIZE[1] * SCALE
+
+# def grid_to_world(row, col) -> Tuple[int, int]:
+#     return col * TILE_SIZE[0] * SCALE, row * TILE_SIZE[1] * SCALE
